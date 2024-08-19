16c5ffd3c13ae6169c36dbdc5366e7d7ec9c0b5d
asqel <91128487+asqel@users.noreply.github.com>
2024-08-19 18:45:33 +0200

    update
    yavait un if en trop et ya un flag en plus maintenant whyn't

--- build_profan.sh
+++ build_profan.sh
@@ -1,5 +1,5 @@
 profan_path="../profanOS"
-CFLAGS="-ffreestanding -fno-exceptions -m32 -I$profan_path/include/zlibs -I$profan_path/include/addons -O1 -nostdinc -U_WIN32 -U__linux__ -Iinclude -Isrc -D__profanOS__=1"
+CFLAGS="-fPIC -ffreestanding -fno-exceptions -m32 -I$profan_path/include/zlibs -I$profan_path/include/addons -O1 -nostdinc -U_WIN32 -U__linux__ -Iinclude -Isrc -D__profanOS__=1"
 
 LDFLAGS="-shared -nostdlib -m32"
 LD=gcc
@@ -16,15 +16,11 @@ mkdir -p $bins
 files="src/*.c src/atomic/*.c src/audio/*.c src/cpuinfo/*.c src/events/*.c src/file/*.c src/haptic/*.c src/joystick/*.c src/power/*.c src/render/*.c src/render/software/*.c src/stdlib/*.c src/thread/*.c src/timer/*.c src/video/*.c src/audio/disk/*.c src/audio/dummy/*.c src/filesystem/profanOS/*.c src/video/profanOS/*.c src/haptic/dummy/*.c src/joystick/dummy/*.c src/main/dummy/*.c src/thread/generic/*.c src/timer/profanOS/*.c src/loadso/dummy/*.c"
 count=0
 for i in $files; do
-    if [[ "$i" != "$prefix/src/joystick/"* ]]; then
-        echo "Compiling $i..."
-        $CC $CFLAGS -c $i -o $bins/$count.o
-        if [ ! -f $bins/$count.o ]; then
-            echo "Cannot compile $i"
-            exit 1
-        fi
-    else
-        echo "Skipping $i"
+    echo "Compiling $i..."
+    $CC $CFLAGS -c $i -o $bins/$count.o
+    if [ ! -f $bins/$count.o ]; then
+        echo "Cannot compile $i"
+        exit 1
     fi
     count=$((count + 1))
 done
